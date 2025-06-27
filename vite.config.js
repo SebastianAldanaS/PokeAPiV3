f830eefa3ca6ba0009422a4ec5f65b9f711dc02f
Sebastian Aldana S. <sebas.a.s1313@hotmail.com>
2025-06-27 15:59:06 -0500

    vercel

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,21 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
     port: 3000,
   },
-  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router-dom']
+        }
+      }
+    }
+  },
+  base: './'
 })
